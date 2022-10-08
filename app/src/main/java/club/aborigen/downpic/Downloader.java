d4a3a98dc203724b5b94570bc9a5cbcfffac0441
Henrik Gardishyan <henrik.gardishyan@gmail.com>
2022-10-09 02:40:04 +0400

    Sync added

--- app/src/main/java/club/aborigen/downpic/Downloader.java
+++ app/src/main/java/club/aborigen/downpic/Downloader.java
@@ -25,37 +25,39 @@ public class Downloader extends Thread {
     @Override
     public void run() {
         super.run();
-        Log.i(TAG, "Downloader thread started: " + address);
+        Log.i(TAG, "Downloader thread started: " + getId());
 
-        try {
-            long ms1 = System.currentTimeMillis();
-            URL url = new URL(address);
-            URLConnection connection = url.openConnection();
-            Log.i(TAG, "Trying to connect: " + address);
+        synchronized (listener) {
 
-            connection.connect();
-            int contentLength = connection.getContentLength();
-            Log.i(TAG, "Connected: " + address + ", expected length: " + contentLength);
+            try {
+                long ms1 = System.currentTimeMillis();
+                URL url = new URL(address);
+                URLConnection connection = url.openConnection();
+                Log.i(TAG, "Trying to connect: " + address);
 
-            InputStream input = new BufferedInputStream(url.openStream(), 8192);
+                connection.connect();
+                int contentLength = connection.getContentLength();
+                Log.i(TAG, "Connected: " + address + ", expected length: " + contentLength);
 
-            ByteArrayOutputStream output = new ByteArrayOutputStream(contentLength);
+                InputStream input = new BufferedInputStream(url.openStream(), 8192);
 
-            byte[] chunk = new byte[1024];
-            int count = input.read(chunk);
-            while (count > 0) {
-                output.write(chunk, 0, count);
-                count = input.read(chunk);
-                listener.onBitmapProgress((output.size() * 100) / contentLength);
-                Thread.sleep(100); 
+                ByteArrayOutputStream output = new ByteArrayOutputStream(contentLength);
+
+                byte[] chunk = new byte[1024];
+                int count = input.read(chunk);
+                while (count > 0) {
+                    output.write(chunk, 0, count);
+                    count = input.read(chunk);
+                    listener.onBitmapProgress((output.size() * 100) / contentLength);
+                    Thread.sleep(100);
+                }
+                Bitmap bmp = BitmapFactory.decodeByteArray(output.toByteArray(), 0, output.size());
+                Log.i(TAG, "Download finished: " + output.size());
+
+                listener.onBitmapDownloaded(bmp, contentLength, System.currentTimeMillis() - ms1);
+            } catch (Exception e) {
+                Log.e(TAG, "Downloader error: " + e);
             }
-
-            Bitmap bmp = BitmapFactory.decodeByteArray(output.toByteArray(), 0, output.size());
-            Log.i(TAG, "Download finished: " + output.size());
-
-            listener.onBitmapDownloaded(bmp, contentLength, System.currentTimeMillis() - ms1);
-        } catch (Exception e) {
-            Log.e(TAG, "Downloader error: " + e);
         }
     }
 
